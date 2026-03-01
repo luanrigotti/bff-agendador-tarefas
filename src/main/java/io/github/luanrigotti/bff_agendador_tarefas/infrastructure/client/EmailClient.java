a5817c15cc1c4b855add3854dacf30ca37509dc7
Luan <luanrigotti49@gmail.com>
2026-02-28 23:33:24 -0300

    Adicionado cron

--- src/main/java/io/github/luanrigotti/bff_agendador_tarefas/infrastructure/client/EmailClient.java
+++ src/main/java/io/github/luanrigotti/bff_agendador_tarefas/infrastructure/client/EmailClient.java
@@ -2,10 +2,12 @@ package io.github.luanrigotti.bff_agendador_tarefas.infrastructure.client;
 
 import io.github.luanrigotti.bff_agendador_tarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     Void enviarEmail(@RequestBody TarefasDTOResponse dto);
 }
